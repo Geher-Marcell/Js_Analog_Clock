5884a8de5ed691ab3c9379378415e15ea6e0c96a
Geher-Marcell <geher.marcell@students.jedlik.eu>
2024-03-11 13:50:18 +0100

    Add dynamic time display and update clock hands

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 setInterval(() => {
     let date = new Date();
 
@@ -11,7 +9,21 @@ setInterval(() => {
     let minutesDeg = (minutes / 60) * 360;
     let hoursDeg = (hours / 12) * 360;
 
-    document.getElementById('second').style.transform = `rotate(${secondsDeg}deg)`;
-    document.getElementById('minute').style.transform = `rotate(${minutesDeg}deg)`;
-    document.getElementById('hour').style.transform = `rotate(${hoursDeg}deg)`;
+    // clock hands
+    document.querySelector('#second').style.transform = `rotate(${secondsDeg}deg)`;
+    document.querySelector('#minute').style.transform = `rotate(${minutesDeg}deg)`;
+    document.querySelector('#hour').style.transform = `rotate(${hoursDeg}deg)`;
+
+    document.getElementById('time').innerHTML = `${hours}:${minutes}:${seconds}`;
+
+    document.querySelectorAll('#second>div>p').forEach((el) => {
+        el.innerHTML = seconds;
+        el.style.transform = `rotate(-${secondsDeg}deg)`;
+    });
+    document.querySelectorAll('#minute>div>p').forEach((el) => {
+        el.innerHTML = minutes;
+    });
+    document.querySelectorAll('#hour>div>p').forEach((el) => {
+        el.innerHTML = hours;
+    });
 }, 1000);
