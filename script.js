3e11c222ae7c58339af11de1186a3f345a9559bd
Geher-Marcell <geher.marcell@students.jedlik.eu>
2024-03-12 11:01:07 +0100

    Default clock code pure js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let clockStats = [
+    {
+        id: 'second',
+        numCount: 14,
+        oneRot: 60
+    },
+    {
+        id: 'minute',
+        numCount: 10,
+        oneRot: 60
+    },
+    {
+        id: 'hour',
+        numCount: 6,
+        oneRot: 12
+    }
+]
+
+CreateClock();
+
 setInterval(() => {
     let date = new Date();
 
@@ -14,16 +34,55 @@ setInterval(() => {
     document.querySelector('#minute').style.transform = `rotate(${minutesDeg}deg)`;
     document.querySelector('#hour').style.transform = `rotate(${hoursDeg}deg)`;
 
-    document.getElementById('time').innerHTML = `${hours}:${minutes}:${seconds}`;
-
     document.querySelectorAll('#second>div>p').forEach((el) => {
         el.innerHTML = seconds;
-        el.style.transform = `rotate(-${secondsDeg}deg)`;
+        el.style.transform = `rotate(-${180+secondsDeg}deg)`;
     });
     document.querySelectorAll('#minute>div>p').forEach((el) => {
         el.innerHTML = minutes;
+        el.style.transform = `rotate(-${180+minutesDeg}deg)`;
     });
     document.querySelectorAll('#hour>div>p').forEach((el) => {
         el.innerHTML = hours;
+        el.style.transform = `rotate(-${180+hoursDeg}deg)`;
     });
 }, 1000);
+
+function CreateClock(){   
+    for(let i = 0; i < clockStats.length; i++){
+        let pointer = document.createElement('div');
+        pointer.id = clockStats[i].id;
+
+        //Default styling
+        pointer.style.width = "10px";
+        pointer.style.height = "10px";
+        pointer.style.position = "absolute";
+        pointer.style.top = "50%";
+        pointer.style.left = "50%";
+        pointer.style.transform = "translate(-50%, -50%)";
+
+        let num = document.createElement('div');
+
+        //Default styling
+        num.style.display = "grid";
+        num.style.gridTemplateRows = "1fr 1fr";
+        num.style.marginTop = "20px";
+
+        pointer.appendChild(num);
+
+        for(let j = 0; j < clockStats[i].numCount; j++){
+            let p = document.createElement('p');
+            p.classList.add('clockNum');
+
+            //Default styling
+            p.style.padding = 0;
+            p.style.margin = 0;
+
+            num.appendChild(p);
+        }
+
+        document.querySelector("#clock").appendChild(pointer);
+
+        document.querySelector("#clock").style.position = "relative";
+    }
+}
